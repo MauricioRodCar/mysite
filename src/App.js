b0b015e3bc5e65ffa8a9a3e609404dfbdb20de0a
Mauricio Rodríguez <mauriciorodricar@hotmail.com>
2022-02-23 15:18:05 -0600

    Restricted to portraitmode when using phone

--- src/App.js
+++ src/App.js
@@ -2,17 +2,42 @@ import Routes from './routes';
 import { Router } from 'react-router-dom';
 import './App.css';
 import Layout from "./components/Layout"
+import { useState, useLayoutEffect} from 'react';
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
 
-function App() {
+
+
+const App = () => {
+
+  const [isPhone,setIsPhone] = useState(window.matchMedia("(max-height: 415px) and (max-width: 850px)").matches)
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setIsPhone(window.innerWidth <= 850 && window.innerHeight <= 415);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
   return (
-    <Router  history={history}>
-      <Layout>
-        <Routes/>
-      </Layout>
-    </Router>
+    <>
+    {
+      isPhone ? (
+        <div className="rotate-alert">
+          <p className="rotate-text">Please rotate your device</p>
+        </div>
+      )
+      :
+      (<Router  history={history}>
+        <Layout>
+          <Routes/>
+        </Layout>
+      </Router>)
+    }
+    </>
   );
 }
 
